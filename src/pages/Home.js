14b4b5003916732bf55ee7f5dc620d3450fe0c98
harshitbadolla <harshitbadolla@yahoo.com>
2022-03-16 23:32:30 +0530

    added background image

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,12 @@
 import { Videos } from "../components/Videos";
+import "../"
 import chessBanner from "../images/chessBanner.jpeg";
 
 export function Home() {
+    // <img src={chessBanner} className="lg:w-full lg:h-144" alt="chess Banner"/>
     return (
         <>
-            <div>
-                <img src={chessBanner} className="lg:w-full lg:h-144" alt="chess Banner"/>
-            </div>
+            <div className="banner-img"></div>
 
             <div className="my-container">
                 <h1 className="font-bold mt-5 mb-5 text-3xl">
